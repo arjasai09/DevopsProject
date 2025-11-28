89709c56c995b680cd0ae8636cac38ebb5c27502
Sai Arja <arjasai09@gmail.com>
2025-11-28 11:25:18 +0530

    updating the  code 11/28

--- Cloning.py
+++ Cloning.py
@@ -1,17 +1,41 @@
-print("Hello")
+# print("Hello")
 
 
 
-class MyProgram():
-    def firstProgram(self, Name, Branch, Collge):
-        self.Name = Name
-        self.Branch = Branch
-        self.Collge = Collge
+# class MyProgram():
+#     def firstProgram(self, Name, Branch, Collge):
+#         self.Name = Name
+#         self.Branch = Branch
+#         self.Collge = Collge
 
-    def Marks(self, marks):
-        self.marks = marks
-        return f"{(self.marks*100)/600}"
+#     def Marks(self, marks):
+#         self.marks = marks
+#         return f"{(self.marks*100)/600}"
 
-MyP= MyProgram()
-MyP.firstProgram("Sai Kiran", "Automobile", "VKR &VNB") 
-print(MyP.Marks(522))
+# MyP= MyProgram()
+# MyP.firstProgram("Sai Kiran", "Automobile", "VKR &VNB") 
+# print(MyP.Marks(522))
+
+
+# list = [23, 4,56,76,45,7]
+
+# start = 0
+# end = len(list)-1
+# for i in range (len(list)):
+#     if (start < end):
+#         list[i] > list[i+1]
+#         list[i]
+
+
+# import time
+
+# start = time.time()
+
+# for i in range(1, 101):
+#     print(i)
+
+# print(time.time()-start)
+
+n = 10
+for i in range(n // 2, n + 1):
+    print(int(i))
